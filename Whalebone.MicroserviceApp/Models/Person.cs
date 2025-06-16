fa18f0d2afeaecc4278977e46bb5d3083ade6a19
Edu Vaiana <eduardo.vaiana@copeland.com>
2025-06-17 00:47:12 +0200

    Add project files.

---
+++ Whalebone.MicroserviceApp/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Whalebone.MicroserviceApp.Models;
+
+public class Person
+{
+    public Guid ExternalId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
