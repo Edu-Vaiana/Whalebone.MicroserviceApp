757f7dacea9532c6805392abfbe2455ed7ab01a7
Edu Vaiana <eduardo.vaiana@copeland.com>
2025-06-17 02:36:32 +0200

    Add migration for Person entity

---
+++ Whalebone.MicroserviceApp/Migrations/20250616235437_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Whalebone.MicroserviceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    ExternalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.ExternalId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
--- Whalebone.MicroserviceApp/Models/Person.cs
+++ Whalebone.MicroserviceApp/Models/Person.cs
@@ -1,7 +1,10 @@
-﻿namespace Whalebone.MicroserviceApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Whalebone.MicroserviceApp.Models;
 
 public class Person
 {
+    [Key]
     public Guid ExternalId { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
